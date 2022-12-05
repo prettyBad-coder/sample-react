246ca4855a43a200ede8835021126d07627df53e
Oskar Jaworski <oskiflog@gmail.com>
2022-12-05 23:49:40 +0100

    Router: Add /products as private route and add simple 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import AboutShop from "./routes/AboutShop.";
 import StationaryShops from "./routes/StationaryShops";
 import Contact from "./routes/Contact";
 import Products from "./routes/Products";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
 
@@ -13,11 +14,14 @@ function App() {
         <Routes>
             <Route element={ <DefaultLayout/> }>
                 <Route path="/" element={ <MainPage/> }/>
-                <Route path="/products" element={ <Products/> }/>
                 <Route path="/about-shop" element={ <AboutShop/> }/>
                 <Route path="/stationary-shops" element={ <StationaryShops/> }/>
                 <Route path="/contact" element={ <Contact/> }/>
                 <Route path="/login" element={ <LoginPage/> }/>
+                <Route element={ <PrivateRoute/> }>
+                    <Route path="/products" element={ <Products/> }/>
+                </Route>
+                <Route path="*" element={ <div>404</div> }/>
             </Route>
         </Routes>
     );
